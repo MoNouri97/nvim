9942875556976f1a31d75efd28a69889a41e96b3
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2023-10-25 19:29:14 +0200

    merge

--- lua/plugins/myconfig.lua
+++ lua/plugins/myconfig.lua
@@ -1,6 +1,35 @@
 return {
   { "nvimtools/none-ls.nvim", enabled = false },
-  { "stevearc/conform.nvim",  enabled = false },
+  { "stevearc/conform.nvim", enabled = false },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
+  { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        omnisharp = {
+          handlers = {
+            ["textDocument/definition"] = function(...)
+              return require("omnisharp_extended").handler(...)
+            end,
+          },
+          keys = {
+            {
+              "gd",
+              function()
+                require("omnisharp_extended").telescope_lsp_definitions()
+              end,
+              desc = "Goto Definition",
+            },
+          },
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
+        },
+        gdscript = {},
+      },
+    },
+  },
   {
     "rcarriga/nvim-notify",
     opts = {
@@ -22,11 +51,19 @@ return {
       styles = {
         -- Background styles. Can be "dark", "transparent" or "normal"
         sidebars = "transparent", -- style for sidebars, see below
-        floats = "transparent",   -- style for floating windows
+        floats = "transparent", -- style for floating windows
       },
+      -- transparent = true,
+      -- styles = {
+      --   -- Background styles. Can be "dark", "transparent" or "normal"
+      --   sidebars = "transparent", -- style for sidebars, see below
+      --   floats = "transparent", -- style for floating windows
+      -- },
     },
   },
   {
     "habamax/vim-godot",
+    lazy = true,
   },
+  { "ThePrimeagen/vim-be-good" },
 }
