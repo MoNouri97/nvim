9313e424e1f7068b2de20dccc39ad67a4eb772ba
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2024-01-08 17:53:15 +0100

    debug off

--- init.lua
+++ init.lua
@@ -2,4 +2,5 @@
 require("config.lazy")
 -- load snippets from path/of/your/nvim/config/vsc
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./vsc" } })
-vim.lsp.set_log_level("debug")
+-- vim.lsp.set_log_level("debug")
+vim.lsp.set_log_level("off")
