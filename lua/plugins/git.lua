49636511e370c74f8e808135ee762aef8e9aaa42
Mohamed Nouri <mohamed@memomeister.com>
2024-01-26 10:19:37 +0100

    grouped git plugins

---
+++ lua/plugins/git.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "ThePrimeagen/git-worktree.nvim",
+    keys = function()
+      return {
+        {
+          "<leader>gw",
+          function()
+            require("telescope").extensions.git_worktree.git_worktrees()
+          end,
+          mode = "n",
+          desc = "Git Worktrees",
+        },
+      }
+    end,
+  },
+  { "lewis6991/gitsigns.nvim", opts = {
+    current_line_blame = true,
+  } },
+}
