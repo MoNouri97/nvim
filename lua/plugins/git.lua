72ef382bf05c88e7d2853d1439f79e607ec65827
Mohamed Nouri <mohamed@memomeister.com>
2024-02-06 14:59:39 +0100

    telescope & worktree changes

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,21 +2,59 @@ return {
   {
     "tpope/vim-fugitive",
   },
+  { "lewis6991/gitsigns.nvim", opts = { current_line_blame = true } },
+  { "sindrets/diffview.nvim" },
   {
     "ThePrimeagen/git-worktree.nvim",
     keys = function()
+      require("telescope").load_extension("git_worktree")
       return {
         {
           "<leader>gw",
-          function()
-            require("telescope").extensions.git_worktree.git_worktrees()
-          end,
+          "<cmd>Telescope git_worktree theme=dropdown<cr>",
+          -- function()
+          --   require("telescope").extensions.git_worktree.git_worktrees()
+          -- end,
           mode = "n",
           desc = "Git Worktrees",
         },
       }
     end,
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
   },
-  { "lewis6991/gitsigns.nvim", opts = { current_line_blame = true } },
-  { "sindrets/diffview.nvim" },
+  {
+    "aaronhallaert/advanced-git-search.nvim",
+    keys = {
+      { "<leader>gS", "<cmd>AdvancedGitSearch<cr>", desc = "Advanced Git Search" },
+      { "<leader>gf", "<cmd>AdvancedGitSearch diff_commit_file<cr>", desc = "File Git History" },
+      {
+        mode = { "v" },
+        "<leader>gl",
+        "<cmd>'<,'>AdvancedGitSearch diff_commit_line<cr>",
+        desc = "Line Git History",
+      },
+    },
+    config = function()
+      -- optional: setup telescope before loading the extension
+      require("telescope").setup({
+        -- move this to the place where you call the telescope setup function
+        extensions = {
+          advanced_git_search = {
+            show_builtin_git_pickers = false,
+            entry_default_author_or_date = "author", -- one of "author" or "date"
+            -- Telescope layout setup
+            telescope_theme = {
+              show_custom_functions = "dropdown",
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("advanced_git_search")
+    end,
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
     "nvim-telescope/telescope.nvim",
     opts = function(_, opts)
       opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        -- preview = false,
         layout_strategy = "horizontal",
         layout_config = {
           -- height = 0.95,
@@ -26,38 +27,5 @@ return {
         },
       })
     end,
-  },
-  {
-    "aaronhallaert/advanced-git-search.nvim",
-    keys = {
-      { "<leader>gS", "<cmd>AdvancedGitSearch<cr>", desc = "Advanced Git Search" },
-      { "<leader>gf", "<cmd>AdvancedGitSearch diff_commit_file<cr>", desc = "File Git History" },
-      {
-        mode = { "v" },
-        "<leader>gl",
-        "<cmd>'<,'>AdvancedGitSearch diff_commit_line<cr>",
-        desc = "Line Git History",
-      },
-    },
-    config = function()
-      -- optional: setup telescope before loading the extension
-      require("telescope").setup({
-        -- move this to the place where you call the telescope setup function
-        extensions = {
-          advanced_git_search = {
-            show_builtin_git_pickers = false,
-            entry_default_author_or_date = "author", -- one of "author" or "date"
-            -- Telescope layout setup
-            telescope_theme = {
-              show_custom_functions = "dropdown",
-            },
-          },
-        },
-      })
-      require("telescope").load_extension("advanced_git_search")
-    end,
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
   },
 }
