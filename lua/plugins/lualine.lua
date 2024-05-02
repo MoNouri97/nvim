ddf74a09b188844c5e2cb5f6c4a8822400e5c1d4
Mohamed Nouri <mohamed@memomeister.com>
2024-05-02 15:06:02 +0200

    mac: cleanup

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,19 +6,14 @@ return {
       component_separators = "",
       section_separators = { left = "", right = "" },
     }
-    opts.sections.lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } }
+    -- +-------------------------------------------------+
+    -- | A | B | C                             X | Y | Z |
+    -- +-------------------------------------------------+
+    opts.sections.lualine_a = { { "mode", separator = { left = "", right = "" } } }
     opts.sections.lualine_y = { "filetype", "encoding" }
     opts.sections.lualine_z = {
       { "progress", separator = "", padding = { left = 0 } },
       { "location", separator = { right = "" }, padding = { left = 0 } },
-    }
-    opts.inactive_sections = {
-      lualine_a = { "filename" },
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = { "location" },
     }
   end,
 }
