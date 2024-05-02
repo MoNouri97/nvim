b2e41c18b4c59bacaa872dc29f99542c41cee1d2
Mohamed Nouri <mohamed@memomeister.com>
2024-05-02 15:39:36 +0200

    mac: git blame

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,7 +2,22 @@ return {
   {
     "tpope/vim-fugitive",
   },
-  { "lewis6991/gitsigns.nvim", opts = { current_line_blame = true } },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter_opts = {
+        relative_time = true,
+      },
+    },
+  },
   {
     "ThePrimeagen/git-worktree.nvim",
     config = function()
