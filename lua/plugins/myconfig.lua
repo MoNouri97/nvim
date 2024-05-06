cf44219b4f0d50d8fce3873d93b4b2832fde1289
Mohamed Nouri <mohamed@memomeister.com>
2024-05-06 10:45:19 +0200

    mac: removed neotree

--- lua/plugins/myconfig.lua
+++ lua/plugins/myconfig.lua
@@ -18,5 +18,5 @@ return {
   },
   { "ThePrimeagen/vim-be-good" },
   { "akinsho/bufferline.nvim", enabled = false },
-  -- { "nvim-neo-tree/neo-tree.nvim", enabled = true },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
