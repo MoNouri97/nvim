99fda9d88ec6a9ee761af4f5c0403ff578e0e89c
Mohamed Nouri <mohamed@memomeister.com>
2025-02-17 17:07:40 +0100

    mac: harpoon + side oil

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,171 +1,116 @@
-if true then
-  return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    enabled = true,
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      settings = {
-        save_on_toggle = true,
-      },
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  enabled = true,
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    settings = {
+      save_on_toggle = true,
     },
-    keys = function()
-      local harpoon = require("harpoon")
-      local conf = require("telescope.config").values
+  },
+  keys = function()
+    local harpoon = require("harpoon")
+    local conf = require("telescope.config").values
 
-      local toggle_opts = {
-        border = "rounded",
-        title_pos = "center",
-        ui_width_ratio = 0.40,
-      }
+    local toggle_opts = {
+      border = "rounded",
+      title_pos = "center",
+      ui_width_ratio = 0.40,
+    }
 
-      local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
-        end
-
-        require("telescope.pickers")
-          .new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-              results = file_paths,
-            }),
-            previewer = conf.file_previewer({}),
-            sorter = conf.generic_sorter({}),
-          })
-          :find()
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
       end
 
-      return {
-        {
-          mode = "n",
-          "<leader>a",
-          function()
-            harpoon:list():add()
-          end,
-          desc = "[A]dd to harpoon",
-        },
-        {
-          mode = "n",
-          "<C-e>",
-          function()
-            harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts)
-          end,
-          desc = "Harpoon menu",
-        },
-        {
-          mode = "n",
-          "<leader>(",
-          function()
-            toggle_telescope(harpoon:list())
-          end,
-          desc = "Harpoon telescope menu",
-        },
-        {
-          mode = "n",
-          "<leader>&",
-          function()
-            harpoon:list():select(1)
-          end,
-          desc = "Harpoon [1]",
-        },
-        {
-          mode = "n",
-          "<leader>é",
-          function()
-            harpoon:list():select(2)
-          end,
-          desc = "Harpoon [2]",
-        },
-        {
-          mode = "n",
-          '<leader>"',
-          function()
-            harpoon:list():select(3)
-          end,
-          desc = "Harpoon [3]",
-        },
-        {
-          mode = "n",
-          "<leader>'",
-          function()
-            harpoon:list():select(4)
-          end,
-          desc = "Harpoon [4]",
-        },
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
 
-        -- Toggle previous & next buffers stored within Harpoon list
-        -- {
-        --   mode = "n",
-        --   "<C-J>",
-        --   function()
-        --     harpoon:list():prev()
-        --   end,
-        --   desc = "Harpoon Prev",
-        -- },
-        -- {
-        --   mode = "n",
-        --   "<C-K>",
-        --   function()
-        --     harpoon:list():next()
-        --   end,
-        --   desc = "Harpoon Next",
-        -- },
-      }
-    end,
-  }
-end
+    return {
+      {
+        mode = "n",
+        "<leader>a",
+        function()
+          harpoon:list():add()
+        end,
+        desc = "[A]dd to harpoon",
+      },
+      {
+        mode = "n",
+        "<C-e>",
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts)
+        end,
+        desc = "Harpoon menu",
+      },
+      {
+        mode = "n",
+        "<leader>(",
+        function()
+          toggle_telescope(harpoon:list())
+        end,
+        desc = "Harpoon telescope menu",
+      },
+      {
+        mode = "n",
+        "<leader>&",
+        function()
+          harpoon:list():select(1)
+        end,
+        desc = "Harpoon [1]",
+      },
+      {
+        mode = "n",
+        "<leader>é",
+        function()
+          harpoon:list():select(2)
+        end,
+        desc = "Harpoon [2]",
+      },
+      {
+        mode = "n",
+        '<leader>"',
+        function()
+          harpoon:list():select(3)
+        end,
+        desc = "Harpoon [3]",
+      },
+      {
+        mode = "n",
+        "<leader>'",
+        function()
+          harpoon:list():select(4)
+        end,
+        desc = "Harpoon [4]",
+      },
 
--- return {
---   "ThePrimeagen/harpoon",
---   branch = "master",
---   keys = function()
---     local mark = require("harpoon.mark")
---     local ui = require("harpoon.ui")
---     return {
---       { "<leader>a", mark.add_file, mode = "n", desc = "add to harpoon" },
---       { "<C-e>", ui.toggle_quick_menu, mode = "n" },
---       {
---         "<leader>&",
---         function()
---           ui.nav_file(1)
---         end,
---         mode = "n",
---         desc = "Harpoon goto 1",
---       },
---       {
---         "<leader>é",
---         function()
---           ui.nav_file(2)
---         end,
---         mode = "n",
---         desc = "Harpoon goto 2",
---       },
---       {
---         '<leader>"',
---         function()
---           ui.nav_file(3)
---         end,
---         mode = "n",
---         desc = "Harpoon goto 3",
---       },
---       {
---         "<leader>'",
---         function()
---           ui.nav_file(4)
---         end,
---         mode = "n",
---         desc = "Harpoon goto 4",
---       },
---       {
---         "<C-k>",
---         function()
---           ui.nav_next()
---         end,
---         mode = "n",
---         desc = "Harpoon next",
---       },
---     }
---   end,
--- }
+      -- Toggle previous & next buffers stored within Harpoon list
+      {
+        mode = "n",
+        "<leader>j",
+        function()
+          harpoon:list():prev({ ui_nav_wrap = true })
+        end,
+        desc = "Harpoon Prev",
+      },
+      {
+        mode = "n",
+        "<leader>k",
+        function()
+          harpoon:list():next({ ui_nav_wrap = true })
+        end,
+        desc = "Harpoon Next",
+      },
+    }
+  end,
+}
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,24 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "oil",
   callback = function()
+    local oil_buf = vim.api.nvim_get_current_buf()
     vim.opt_local.colorcolumn = ""
+    vim.keymap.set("n", "q", ":q<CR>", { buffer = true, silent = true })
+    vim.keymap.set("n", "<CR>", function()
+      -- Get the selected file path from Oil
+      local oil = require("oil")
+      local entry = oil.get_cursor_entry()
+
+      if entry and entry.type ~= "directory" then
+        -- Open the file
+        oil.select()
+        -- Close any other splits except the main one and oil
+        vim.cmd("only")
+      else
+        -- If it's a directory, just use default Oil behavior
+        oil.select()
+      end
+    end, { buffer = oil_buf, silent = true })
   end,
 })
 
@@ -12,6 +29,20 @@ return {
     opts = {},
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          vim.cmd("vsplit | wincmd r | vertical resize 30")
+          require("oil").open()
+          -- Get the buffer number of the Oil window
+          -- local oil_buf = vim.api.nvim_get_current_buf()
+          -- Create buffer-local keymap
+          -- vim.keymap.set("n", "q", ":q<CR>", { buffer = oil_buf, silent = true })
+        end,
+        desc = "Side Oil",
+      },
+    },
     config = function()
       require("oil").setup({
         use_default_keymaps = false,
