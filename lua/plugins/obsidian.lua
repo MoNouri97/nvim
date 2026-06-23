a0fd0ac732eb21daf34a5322e905fdda641a58dc
Mohamed Nouri <mohamed@memomeister.com>
2026-06-23 15:24:49 +0100

    mac: markdown

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,53 +1,65 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 
-    -- see below for full list of optional dependencies 👇
-  },
-  keys = {
-    { "<C-l>", "<cmd>ObsidianToggleCheckbox<cr>", desc = "Obsidian Toggle Checkbox", mode = { "n", "i" } },
-    { "<leader>oq", "<cmd>ObsidianQuickSwitch<cr>", desc = "Obsidian Quick Switch" },
-    { "<leader>od", "<cmd>ObsidianDailies<cr>", desc = "Obsidian Dailies" },
-    {
-      "<leader>ox",
-      function()
-        vim.ui.input({ prompt = "Note title: " }, function(title)
-          if title then
-            vim.cmd("ObsidianExtractNote " .. title)
-          end
-        end)
-      end,
-      desc = "Obsidian Extract Note",
-      mode = "v",
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
     },
-  },
-  opts = {
-    workspaces = {
+    keys = {
+      { "<C-l>", "<cmd>ObsidianToggleCheckbox<cr>", desc = "Obsidian Toggle Checkbox", mode = { "n", "i" } },
+      { "<leader>oq", "<cmd>ObsidianQuickSwitch<cr>", desc = "Obsidian Quick Switch" },
+      { "<leader>od", "<cmd>ObsidianDailies<cr>", desc = "Obsidian Dailies" },
       {
-        name = "work",
-        path = vim.fn.has("macunix") == 1 and "~/Documents/obsidian-vault" or "/mnt/D/Dev/obsidian",
+        "<leader>ox",
+        function()
+          vim.ui.input({ prompt = "Note title: " }, function(title)
+            if title then
+              vim.cmd("ObsidianExtractNote " .. title)
+            end
+          end)
+        end,
+        desc = "Obsidian Extract Note",
+        mode = "v",
       },
     },
+    opts = {
+      workspaces = {
+        {
+          name = "work",
+          path = vim.fn.has("macunix") == 1 and "~/Documents/obsidian-vault" or "/mnt/D/Dev/obsidian",
+        },
+      },
 
-    notes_subdir = "MemoMeister/tasks",
+      notes_subdir = "MemoMeister/tasks",
 
-    daily_notes = {
-      folder = "MemoMeister/daily",
+      daily_notes = {
+        folder = "MemoMeister/daily",
+      },
+
+      -- see below for full list of options 👇
     },
-
-    -- see below for full list of options 👇
   },
 }
